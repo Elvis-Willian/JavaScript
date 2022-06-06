70d5ce1ace6e178ea5735ac4fc9d16e3a33c1f31
Elvis Willian <106096876+Elvis-Willian@users.noreply.github.com>
2022-06-06 13:33:34 -0300

    novos ex

---
+++ exercicios/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel}Km/h `)
+if(vel>60) { //condição simples: quando não tem SE NÃO
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Diriga sempre usando o sinto de segurança!`)
