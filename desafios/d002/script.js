d77e65cf6913de342c335b7c53a52938174fe8dd
Elvis Willian <106096876+Elvis-Willian@users.noreply.github.com>
2022-06-08 13:27:48 -0300

    up

--- desafios/d002/script.js
+++ desafios/d002/script.js
@@ -18,23 +18,31 @@ function verificar() {
                 img.setAttribute('src', 'imagens/meninocriança.png')
             } else if (idade < 21) {
                 //Jovem
+                img.setAttribute('src', 'imagens/homemjovem.png')
             } else if (idade < 50) {
                 //Adulto
+                img.setAttribute('src', 'imagens/homemadulto.png')
             } else {
                 //Idoso
-            } 
+            }   img.setAttribute('src', 'imagens/idoso.png')
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
             if (idade >= 0 && idade < 10) {
                 //Criança
+                img.setAttribute('src', 'imagens/meninacriança.png')
             } else if (idade < 21) {
                 //Jovem
+                img.setAttribute('src', 'imagens/mulherjovem.png')
             } else if (idade < 50) {
                 //Adulto
+                img.setAttribute('src', 'imagens/mulheradulta.png')
             } else {
                 //Idoso
+                img.setAttribute('src', 'imagens/idosa.png')
             } 
         }
+        res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
