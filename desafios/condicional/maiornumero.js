ea85a10d4621c716784cb74b30c0c7a54e61edda
Elvis Willian <106096876+Elvis-Willian@users.noreply.github.com>
2022-06-15 13:01:57 -0300

    att

--- desafios/condicional/maiornumero.js
+++ desafios/condicional/maiornumero.js
@@ -3,12 +3,12 @@ function verificar() {
     var n2 = Number.parseInt(document.getElementById("num2").value)
     var n3 = Number.parseInt(document.getElementById("num3").value)
 
-    var maior = n1
+    if( n1 > n2 || n1 > n3) {
+        res.innerHTML= (`O número ${n1} é maior`)
+    } else if ( n2 > n1 || n2 > n3) {
+        res.innerHTML= (`O número ${n2} é maior`)
+    } else {
+        res.innerHTML= (`O número ${n3} é maior`)
+    }
 
-    if(n2 > maior)
-    maior = n2
-    else if (n3 > maior)
-    maior = n3
-
-    res.innerHTML= (`O número ${maior} é maior`)
 }
