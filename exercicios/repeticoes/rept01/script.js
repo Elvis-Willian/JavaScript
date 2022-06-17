aae8834d1be5bc9f8642a889074877142497a616
Elvis Willian <106096876+Elvis-Willian@users.noreply.github.com>
2022-06-17 11:52:50 -0300

    att

---
+++ exercicios/repeticoes/rept01/script.js
@@ -0,0 +1,13 @@
+function verificar() {
+    var inicio = Number.parseInt(document.getElementById("inicio").value)
+    var fim = Number.parseInt(document.getElementById("fim").value)
+    var passo = Number.parseInt(document.getElementById("passo").value)
+
+    while (inicio <= fim) {
+        passo + 0
+        res.innerHTML=(`${inicio}`)
+    }
+
+        res.style.textAlign = 'center'
+        
+    }
