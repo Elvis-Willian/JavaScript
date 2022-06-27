94ef36efa50f49252a62c802f54c94a5a58a0bb8
Elvis Willian <106096876+Elvis-Willian@users.noreply.github.com>
2022-06-27 14:26:18 -0300

    Update testes.js

--- exercicios/testes.js
+++ exercicios/testes.js
@@ -1,9 +1,1 @@
-function contar() {
-    let num = 1 
-    let res = document.getElementById('res')
-
-    while (num <= 10) {
-        res.innerHTML= 'olá'
-        num++
-    }
-}
+var cores = ["preto" , "branco" , "vermelho"]
